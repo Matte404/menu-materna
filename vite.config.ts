b6011f8f7cb84d2164ad24858c79e372688e0a42
Matte404 <30328787+Matte404@users.noreply.github.com>
2023-10-04 15:35:56 +0000

    PWA test1

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'menu-materna'
+  plugins: [react(), VitePWA()],
+  base: '/menu-materna/'
 })
