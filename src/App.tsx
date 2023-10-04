28d6ca9133a4298a64bcc260eff2a459103e8928
Matte404 <30328787+Matte404@users.noreply.github.com>
2023-10-04 09:22:46 +0000

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { PrimeReactProvider } from "primereact/api";
 
-import "primereact/resources/themes/viva-light/theme.css"; //theme
+import "primereact/resources/themes/md-light-indigo/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
 // import "primeicons/primeicons.css"; //icons
 import "primeflex/primeflex.css"; // flex
