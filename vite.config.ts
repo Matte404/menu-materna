98065e3e1f21ecc4f4d3032de2023098ba0588c5
Matteo Carrano <matteo.carrano@bss.group>
2023-10-12 22:22:31 +0200

    test-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
 
+const pwaConfig: Partial<VitePWAOptions> = {
+  manifest: {
+    name: 'Menu Materna 2023/24',
+    short_name: 'Menu Materna',
+    description: 'Menu scuola materna anno scolastico 2023/2024',
+    theme_color: '#3dff54',
+    icons: [
+      {
+        src: 'pwa-icon/pwa_x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-icon/pwa_x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA(pwaConfig)],
   base: '/menu-materna/'
 })
