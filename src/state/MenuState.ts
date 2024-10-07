4292b2d7742ab9474130a97a5ff786427cba483f
Matteo Carrano <matteo.carrano@bss.group>
2024-10-07 15:56:28 +0200

    fix week calc

--- src/state/MenuState.ts
+++ src/state/MenuState.ts
@@ -103,7 +103,7 @@ const updateMenu = (newDate: Date) => {
 
   //calc weekNr and day in the week
   const momentDate = moment(newDate);
-  const menuWeekNrIndex = (momentDate.week() + 2) % 4;
+  const menuWeekNrIndex = momentDate.week() % 4;
   const dayOftheweekIndex = momentDate.day() - 1;
 
   if (dayOftheweekIndex < 0 || dayOftheweekIndex > 4) {
