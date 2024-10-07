e6aa5871335da28dca028d66f9aadf94e916bf32
Matteo Carrano <matteo.carrano@bss.group>
2024-10-07 15:23:51 +0200

    new menu 24 25

--- src/pages/MenuCalendarPage.tsx
+++ src/pages/MenuCalendarPage.tsx
@@ -1,21 +1,40 @@
 import { Card } from "primereact/card";
 import { ProgressSpinner } from "primereact/progressspinner";
-import MenuList from "../components/MenuList";
-import CalendarComponent from "../components/CalendarComponent";
-import { useMenuState } from "../state/MenuState";
 import { useEffect } from "react";
+import CalendarComponent from "../components/CalendarComponent";
+import MenuList from "../components/MenuList";
+import { useMenuState } from "../state/MenuState";
 
 export const MenuCalendarPage = () => {
-  const minDate = new Date("10/4/2023");
-  const maxDate = new Date("6/1/2024");
+  const minDate = new Date("10/4/2024");
+  const maxDate = new Date("6/1/2025");
   const disabledDates = [
-    "11/1/2023",
-    "12/8/2023",
-    "12/25/2023",
-    "12/26/2023",
-    "1/1/2024",
-    "4/25/2024",
-    "5/1/2024",
+    "12/23/2024",
+    "12/24/2024",
+    "12/27/2024",
+    "12/28/2024",
+    "12/30/2024",
+    "12/31/2024",
+    "01/02/2025",
+    "01/03/2025",
+    "01/04/2025",
+    "04/17/2025",
+    "04/18/2025",
+    "04/19/2025",
+    "05/02/2025",
+    "05/03/2025",
+
+    // Giorni di vacanza stabiliti a livello nazionale
+    "11/01/2024",
+    "12/08/2024",
+    "12/25/2024",
+    "12/26/2024",
+    "01/01/2025",
+    "01/06/2025",
+    "04/21/2025",
+    "04/25/2025",
+    "05/01/2025",
+    "06/02/2025",
   ].map((x) => new Date(x));
 
   const {
@@ -28,6 +47,7 @@ export const MenuCalendarPage = () => {
   } = useMenuState((state) => state);
 
   useEffect(() => {
+    console.log("MenuCalendarPage useEffect", disabledDates);
     initialize({ minDate, maxDate, disabledDates });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -50,7 +70,7 @@ export const MenuCalendarPage = () => {
     >
       <div className="flex flex-row  justify-content-center  ">
         <Card
-          title="Menu materna 2023/2024"
+          title="Menu materna 2024/2025"
           className="p-5 w-12 md:w-8 lg:w-5 shadow-8 my-4 mx-2 border-round-xl"
         >
           <CalendarComponent
--- src/state/MenuState.ts
+++ src/state/MenuState.ts
@@ -1,121 +1,121 @@
+import moment from "moment";
 import { create } from "zustand";
 import Meal from "../model/Meal";
-import moment from "moment";
 
-import menu from "../assets/4w-menu.json";
-import Season from "../model/SeasonEnum";
 import { devtools } from "zustand/middleware";
+import menu from "../assets/4w-2425-menu.json";
+import Season from "../model/SeasonEnum";
 
 const production = process.env.NODE_ENV === "production" ? true : false;
 
 type MenuState = {
-    day: Date;
-    menuItems: Meal[];
-    minDate: Date;
-    maxDate: Date;
-    disabledDates: Date[];
-    disabledWeekDaysIndex: number[];
-    initialized: boolean;
+  day: Date;
+  menuItems: Meal[];
+  minDate: Date;
+  maxDate: Date;
+  disabledDates: Date[];
+  disabledWeekDaysIndex: number[];
+  initialized: boolean;
 };
 
 type MenuActions = {
-    initialize: (initialValue: Partial<MenuState>) => void;
-    setDate: (date: Date) => void;
+  initialize: (initialValue: Partial<MenuState>) => void;
+  setDate: (date: Date) => void;
 };
 
 export const useMenuState = create<MenuState & MenuActions>()(
-    devtools(
-        (set) => ({
-            day: new Date(),
-            menuItems: [],
-            minDate: new Date(-8640000000000000),
-            maxDate: new Date(8640000000000000),
-            disabledDates: [],
-            disabledWeekDaysIndex: [0, 6],
-            initialized: false,
+  devtools(
+    (set) => ({
+      day: new Date(),
+      menuItems: [],
+      minDate: new Date(-8640000000000000),
+      maxDate: new Date(8640000000000000),
+      disabledDates: [],
+      disabledWeekDaysIndex: [0, 6],
+      initialized: false,
 
-            initialize: (initialValue: Partial<MenuState>) =>
-                set(
-                    (state) => {
-                        const newState = { ...state, ...initialValue, initialized: true };
+      initialize: (initialValue: Partial<MenuState>) =>
+        set(
+          (state) => {
+            const newState = { ...state, ...initialValue, initialized: true };
 
-                        //validate date or get next/prev valid date based on disabled dates and disabled week days
-                        newState.day = checkAndFixDate(newState.day, newState);
-                        newState.menuItems = updateMenu(newState.day); // get menu from json
+            //validate date or get next/prev valid date based on disabled dates and disabled week days
+            newState.day = checkAndFixDate(newState.day, newState);
+            newState.menuItems = updateMenu(newState.day); // get menu from json
 
-                        return newState;
-                    },
-                    false,
-                    `initialize`
-                ),
+            return newState;
+          },
+          false,
+          `initialize`
+        ),
 
-            setDate: (date: Date) =>
-                set(
-                    (state) => {
-                        if (!state.initialized)
-                            throw new Error("MenuState not initialized");
+      setDate: (date: Date) =>
+        set(
+          (state) => {
+            if (!state.initialized)
+              throw new Error("MenuState not initialized");
 
-                        //validate date or get next/prev valid date based on disabled dates and disabled week days
-                        const newDate = checkAndFixDate(date, state);
+            //validate date or get next/prev valid date based on disabled dates and disabled week days
+            const newDate = checkAndFixDate(date, state);
 
-                        return {
-                            day: newDate,
-                            menuItems: updateMenu(newDate), //get menu from json
-                        };
-                    },
-                    false,
-                    `setDate`
-                ),
-        }),
-        { name: "menu-state", enabled: !production, log: !production }
-    )
+            return {
+              day: newDate,
+              menuItems: updateMenu(newDate), //get menu from json
+            };
+          },
+          false,
+          `setDate`
+        ),
+    }),
+    { name: "menu-state", enabled: !production, log: !production }
+  )
 );
 
 const checkAndFixDate = (newDate: Date, state: MenuState): Date => {
-    const newDateTimestamp = newDate.getTime();
-    if (
-        state.disabledWeekDaysIndex.includes(newDate.getDay()) ||
-        state.disabledDates.map((x) => x.getTime()).includes(newDateTimestamp)
-    ) {
-        const oldDateTimestamp = state.day.getTime();
-        //check if new date is after or before old date ang do 1 day + or -
-        return checkAndFixDate(
-            newDateTimestamp >= oldDateTimestamp
-                ? new Date(newDateTimestamp + 86400000)
-                : new Date(newDateTimestamp - 86400000),
-            state
-        );
-    }
-    return newDate;
+  const newDateTimestamp = newDate.getTime();
+  if (
+    state.disabledWeekDaysIndex.includes(newDate.getDay()) ||
+    state.disabledDates.map((x) => x.getTime()).includes(newDateTimestamp)
+  ) {
+    const oldDateTimestamp = state.day.getTime();
+    //check if new date is after or before old date ang do 1 day + or -
+    return checkAndFixDate(
+      newDateTimestamp >= oldDateTimestamp
+        ? new Date(newDateTimestamp + 86400000)
+        : new Date(newDateTimestamp - 86400000),
+      state
+    );
+  }
+  return newDate;
 };
 
 const updateMenu = (newDate: Date) => {
-    const month = newDate.getMonth() + 1;
-    const day = newDate.getDate();
+  const month = newDate.getMonth() + 1;
+  const day = newDate.getDate();
 
-    //winter start first november end 15 april
-    //summer start 16 april end 31 october
-    //calc season
-    const season =
-        month < 4 || month >= 11 || (month == 5 && day <= 15)
-            ? Season.Winter
-            : Season.Summer;
+  //winter start first november end 15 april
+  //summer start 16 april end 31 october
+  //calc season
+  const season =
+    month < 4 || month >= 11 || (month == 5 && day <= 15)
+      ? Season.Winter
+      : Season.Summer;
 
-    //calc weekNr and day in the week
-    const momentDate = moment(newDate);
-    const menuWeekNrIndex = momentDate.week() % 4;
-    const dayOftheweekIndex = momentDate.day() - 1;
+  //calc weekNr and day in the week
+  const momentDate = moment(newDate);
+  const menuWeekNrIndex = (momentDate.week() + 2) % 4;
+  const dayOftheweekIndex = momentDate.day() - 1;
 
-    if (dayOftheweekIndex < 0 || dayOftheweekIndex > 4) {
-        return [];
-    }
+  if (dayOftheweekIndex < 0 || dayOftheweekIndex > 4) {
+    return [];
+  }
 
-    //get menu
-    let weekMenu = menu[menuWeekNrIndex][dayOftheweekIndex];
-    //filter by season
-    weekMenu = weekMenu.filter(
-        (x) => x.season == season || x.season == Season.All
-    );
+  //get menu
+  let weekMenu = menu[menuWeekNrIndex][dayOftheweekIndex];
+  //filter by season
+  weekMenu = weekMenu.filter(
+    (x) => x.season == season || x.season == Season.All
+  );
 
-    return weekMenu;
+  return weekMenu;
 };
